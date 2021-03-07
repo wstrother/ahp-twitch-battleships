39cb9261da1cfd03cedbf86690b92c8432d47e5c
Wyatt Strother <wyattstrother@gmail.com>
2021-03-07 12:43:16 -0500

    added firebase init to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCis0BQ3BG4OGZYqswOtuBBnQmJmLBKUd8",
+    authDomain: "battleships-94af5.firebaseapp.com",
+    projectId: "battleships-94af5",
+    storageBucket: "battleships-94af5.appspot.com",
+    messagingSenderId: "182371645879",
+    appId: "1:182371645879:web:f03b1c67900db3cd209032",
+    measurementId: "G-YQ0X5475HL"
+  }
 };
